8a676f1b5d222354f5dd6d467788666aeaa91163
walked22 <walked22@gmail.com>
2020-04-02 17:31:26 -0700

    fixed print line

--- Cluster/collector.py
+++ Cluster/collector.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 while(True):
-	print GPIO.input(4)
+	print(GPIO.input(4))
+	time.sleep(.1)
