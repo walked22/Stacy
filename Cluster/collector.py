386a1801cf13add8073e4f83849b3ec4e91532fc
walked22 <walked22@gmail.com>
2020-04-02 17:34:45 -0700

    First attempt at measuring RPM

--- Cluster/collector.py
+++ Cluster/collector.py
@@ -4,6 +4,12 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while(True):
-	print(GPIO.input(4))
-	time.sleep(.1)
+t0 = time.time()
+
+while(!GPIO.input(4)):
+	t1 = time.time()
+	try:
+		print(1 / ((t1-t0)/60))
+	except ZeroDivisionError:
+		pass
+	t0 = t1
